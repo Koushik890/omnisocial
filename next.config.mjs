706fb122e317c813a7daf84bee35c66118d0e143
Koushik Dey <koushik.dey9806@gmail.com>
2025-01-16 21:10:15 +0530

    Update Next.js image configuration for Instagram CDN

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'scontent.cdninstagram.com',
-      'scontent-iad3-1.cdninstagram.com',
-      'scontent-iad3-2.cdninstagram.com',
-      'instagram.fpnq13-1.fna.fbcdn.net',
-      'graph.instagram.com',
-      'scontent-lga3-1.cdninstagram.com',
-      'scontent-lga3-2.cdninstagram.com',
-      'hebbkx1anhila5yf.public.blob.vercel-storage.com'
-    ],
     remotePatterns: [
       {
         protocol: 'https',
@@ -19,6 +9,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '*.fbcdn.net'
+      },
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com'
       }
     ],
     unoptimized: true
